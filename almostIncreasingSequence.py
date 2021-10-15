6579dd847e747f89b3aba5356686bb5922d6cf41
yazgeldigithub <devster2016@gmail.com>
2021-10-15 16:15:48 -0500

    almost increasing sequence

---
+++ almostIncreasingSequence.py
@@ -0,0 +1,38 @@
+# Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence
+# by removing no more than one element from the array.
+#
+# Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. Sequence containing
+# only one element is also considered to be strictly increasing.
+#
+# Example
+#
+# For sequence = [1, 3, 2, 1], the output should be
+# almostIncreasingSequence(sequence) = false.
+#
+# There is no one element in this array that can be removed in order to get a strictly increasing sequence.
+#
+# For sequence = [1, 3, 2], the output should be
+# almostIncreasingSequence(sequence) = true.
+#
+# You can remove 3 from the array to get the strictly increasing sequence [1, 2]. Alternately, you can remove 2 to
+# get the strictly increasing sequence [1, 3].
+
+
+def almostIncreasingSequence(sequence):
+    i = 0
+    while i < len(sequence) - 1:
+        if not sequence[i] < sequence[i + 1]:
+            if increasingSequence(sequence[:i] + sequence[i+1:]) or \
+                    increasingSequence(sequence[:i+1] + sequence[i+2:]):
+                return True
+            else:
+                return False
+        i += 1
+    return True
+
+
+def increasingSequence(sequence):
+    for i in range(len(sequence) - 1):
+        if not sequence[i] < sequence[i + 1]:
+            return False
+    return True
