3afe5bcd4886996ecb17773b7f60edff0a5d2e2b
yazgeldigithub <devster2016@gmail.com>
2021-10-16 20:20:16 -0500

    single numbers solution

--- fibonacci.py
+++ fibonacci.py
@@ -13,3 +13,14 @@ def fibonacci(n):
 
 
 print(fibonacci(10))  # [1, 1, 2, 3, 5, 8]
+
+
+def fibonacci1(n):  # O(n)
+    lst = [0, 1]  # O(2)
+    for i in range(2, n):  # O(n)
+        lst.append(lst[i - 2] + lst[i - 1])  # O(2)
+
+    return lst[n - 1]  # O(1)
+
+
+print(fibonacci1(10))
