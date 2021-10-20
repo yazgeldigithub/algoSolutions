ceb1137d25209d08c2314f84365123b08ba3ef96
yazgeldigithub <devster2016@gmail.com>
2021-10-20 12:56:20 -0500

    shortest word

---
+++ shortestWord.py
@@ -0,0 +1,9 @@
+#  Given a string of words, return the length of the shortest word(s).
+
+def ShortestWord(input_str):
+    word = map(len, input_str.split())
+    return min(word)
+
+
+print(ShortestWord(
+    "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live"))
