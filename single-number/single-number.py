ba38e59e9f202472875533d9ef7e4f808ce6b047
yazgeldi hojayev <80339236+yazgeldigithub@users.noreply.github.com>
2021-11-27 14:16:06 -0600

    Update single-number.py

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -5,3 +5,23 @@ class Solution:
             res ^= i
         return res
         
+        
+        
+"""
+for i in nums:
+            if nums.count(i) == 1:
+                return i
+                
+################################################                
+                
+hashmap = {}
+    for i in nums:
+        if i not in hashmap:
+            hashmap[i] = 1
+        else:
+            hashmap[i] += 1
+        
+    for key, value in hashmap.items():
+        if value == 1:
+            return key
+"""
